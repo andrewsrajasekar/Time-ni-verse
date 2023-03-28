b7e129c120945ca295b4d4579a287e1bd3cdf286
zd9575 <zd9575@rit.edu>
2023-03-28 11:51:54 -0400

    Basic layout with ListView and TableView displayed on the screen

---
+++ demo/src/main/java/com/timeniverse/taskTracker.java
@@ -0,0 +1,54 @@
+package com.timeniverse;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+import javafx.beans.property.SimpleStringProperty;
+ 
+public class taskTracker {
+   private final SimpleStringProperty assignment = new SimpleStringProperty("");
+   private final SimpleStringProperty duration = new SimpleStringProperty("");
+   private final SimpleStringProperty deadline = new SimpleStringProperty("");
+   private final SimpleStringProperty priority = new SimpleStringProperty("");
+
+
+    public taskTracker() {
+        this("", "", "","");
+    }
+ 
+    public taskTracker(String assignment, String duration, String deadline, String priority) {
+        setAssignment(assignment);
+        setDuration(duration);
+        setDeadline(deadline);
+        setPriority(priority);
+    }
+ 
+    public void setAssignment(String assign) {
+        assignment.set(assign);
+    }
+        
+    public String getDuration() {
+        return duration.get();
+    }
+    
+    public void setDuration(String hours) {
+        duration.set(hours);
+    }
+    
+    public String getDeadline() {
+        return deadline.get();
+    }
+    
+    public void setDeadline(String date) {
+        deadline.set(date);
+    }
+
+    public String getPriority() {
+        return priority.get();
+    }
+    
+    public void setPriority(String isPriority ) {
+        deadline.set(isPriority);
+    }
+
+}
